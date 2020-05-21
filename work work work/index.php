410e2918b150544aaed3ef75aca2b73176c344ae
priyankaghiya <43947284+priyankaghiya@users.noreply.github.com>
2020-05-21 15:35:18 +0530

    Add files via upload
    https://onlinevideosurfing.000webhostapp.com/  is the link for the hosted website

---
+++ work work work/index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/bootstrap-grid.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.5/lib/darkmode-js.min.js"></script>
+  <script>
+  new Darkmode().showWidget();
+</script>
+
+
+    <link rel="shortcut icon" href="../images/total.jpg" type="image/jpg"/>
+
+    <title>Search for your Interest</title>
+    
+  </head>
+
+  <body class="allcontent">
+  
+    <div class="header">
+      <div class="header-data">
+      <div class="alert alert-dark" role="alert">
+  
+      <h1>New Video Sharing Platform </h1>
+</div>
+   
+      </div>
+    </div>
+
+<!-- Block second -->
+<div class="merging">
+  <div class="row">
+    <div class="col-sm-12 col-lg-3">
+      <img class="img-fluid rounded"src="../images/SM.jpg" alt="...">
+  <div class="btn-Center">        
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#SandipMahesvari">Motivational Speaker</button>  
+            </div>
+    </div>
+        
+
+        <div class="col-sm-12 col-lg-3">
+      <img class="img-fluid rounded"src="../images/knowledgegate.jpg" alt="...">
+      <div class="btn-Center">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#KnowledgeGate">Educator</button>  
+            </div>
+    </div>
+        
+
+        <div class="col-sm-12 col-lg-3">
+      <img class="img-fluid rounded"src="../images/aditimittal.jpg" alt="...">
+      <div class="btn-Center">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#adittimital">Comedian</button>  
+            </div>
+    </div>
+        
+
+        <div class="col-sm-12 col-lg-3">
+      <img class="img-fluid rounded"src="../images/teamnach.jpg" alt="...">
+      <div class="btn-Center">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#teamNach">Entertainment</button>  
+            </div>
+    </div>
+        
+  </div>
+</div>
+<!-- <div class="row">
+<img class="round" src="../images/light.jpg" alt="...">
+</div> -->
+
+<div class="footer">
+  <div class="footer-data">
+  <center><h3>Made With ❤️ Priyanka Ghiya</h3></center>
+</div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="SandipMahesvari" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="SandipModel">Motivational Speech BY Sandip Maheshvari</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="embed-responsive embed-responsive-16by9">
+      <iframe width="450" height="315" src="https://www.youtube.com/embed/3gqQevdM7xM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Model 2 -->
+<div class="modal fade" id="KnowledgeGate" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="knowledgeGate">Operating System By Sanchit Jain</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="embed-responsive embed-responsive-16by9">
+         <iframe width="450" height="315" src="https://www.youtube.com/embed/xAvC-MJ_Sz8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Model 3 -->
+
+<div class="modal fade" id="adittimital" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="aditiModel">Hilarious talks by Aditi Mittal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="embed-responsive embed-responsive-16by9">
+           <iframe width="450" height="315" src="https://www.youtube.com/embed/PXSA0ZQr_c4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Model 4 -->
+
+<div class="modal fade" id="teamNach" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="TeamModel">Lamborgini Dance By Team Nach</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="embed-responsive embed-responsive-16by9">
+         <iframe width="450" height="315" src="https://www.youtube.com/embed/4f34A2OrYog" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/main.js">
+  </body>
+</html>
